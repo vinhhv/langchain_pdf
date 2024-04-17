0ee1576bf51189de49aefa76bfbc7b6480b395ea
Vinh Vu <vinhhv@uci.edu>
2024-04-17 10:35:55 -0700

    Add mixin

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ chat = ChatOpenAI(streaming=True)
 prompt = ChatPromptTemplate.from_messages([("human", "{content}")])
 
 
-class StreamingChain(LLMChain):
+class StreamableChain:
     def stream(self, input):
         queue = Queue()
         handler = StreamingHandler(queue)
@@ -44,6 +44,10 @@ class StreamingChain(LLMChain):
             if token is None:
                 break
             yield token
+
+
+class StreamingChain(StreamableChain, LLMChain):
+    pass
 
 
 chain = StreamingChain(llm=chat, prompt=prompt)
