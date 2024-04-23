f4880c0b184d786d2b40165f5a54c43bda8eb694
Vinh Vu <vinhhv@uci.edu>
2024-04-22 21:31:16 -0700

    Aggregate scores

--- app/chat/score.py
+++ app/chat/score.py
@@ -89,5 +89,18 @@ def get_scores():
             'memory': { 'persist_memory': [avg_score] }
         }
     """
+    aggregate = {"llm": {}, "retriever": {}, "memory": {}}
 
-    pass
+    for component_type in aggregate.keys():
+        values = client.hgetall(f"{component_type}_score_values")
+        counts = client.hgetall(f"{component_type}_score_counts")
+
+        names = values.keys()
+
+        for name in names:
+            score = int(values.get(name, 1))
+            count = int(counts.get(name, 1))
+            avg = score / count
+            aggregate[component_type][name] = [avg]
+
+    return aggregate
