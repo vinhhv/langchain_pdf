022c58ae43470ebbc8973fe1303a417fbaf1bedd
Vinh Vu <vinhhv@uci.edu>
2024-04-22 21:50:53 -0700

    Add window buffer memory

---
+++ app/chat/memories/histories/sql_history.py
@@ -0,0 +1,21 @@
+from app.web.api import add_message_to_conversation, get_messages_by_conversation_id
+from langchain.schema import BaseChatMessageHistory
+from pydantic import BaseModel
+
+
+class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
+    conversation_id: str
+
+    @property
+    def messages(self):
+        return get_messages_by_conversation_id(self.conversation_id)
+
+    def add_message(self, message):
+        return add_message_to_conversation(
+            conversation_id=self.conversation_id,
+            role=message.type,
+            content=message.content,
+        )
+
+    def clear(self):
+        pass
